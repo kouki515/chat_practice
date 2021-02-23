281d7d7670f9c431db502271d56f8af2f7c9bcf4
kouki515 <71614240+kouki515@users.noreply.github.com>
2021-02-23 20:09:24 +0900

    チャット機能の実装

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -19,3 +19,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::post('/add', 'HomeController@add')->name('add');
+Route::get('/result/ajax', 'HomeController@getData');
